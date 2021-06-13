33bd8103d6bccf9b904e74dcb0f998bce9a542bd
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-06-13 12:47:14 +0300

    Add files via upload

---
+++ inline.py
@@ -0,0 +1,169 @@
+"""
+ezWhisperBot, Telegram Bot for sending whisper messages
+Copyright (C) 2021  Dash Eclipse
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from typing import Optional
+
+from pyrogram import Client, filters, emoji
+from pyrogram.errors.exceptions.bad_request_400 import (
+    MessageIdInvalid, MessageNotModified
+)
+from pyrogram.types import (
+    User,
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery,
+    ChosenInlineResult
+)
+
+from data import whispers
+
+# https://core.telegram.org/bots/api#answercallbackquery
+# https://core.telegram.org/bots/api#callbackquery
+ANSWER_CALLBACK_QUERY_MAX_LENGTH = 200
+
+# https://www.freeiconspng.com/downloadimg/37535
+WHISPER_ICON_URL = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
+
+
+@Client.on_inline_query()
+async def answer_iq(_, iq: InlineQuery):
+    query = iq.query
+    split = query.split(' ', 1)
+    if query == '' or len(query) > ANSWER_CALLBACK_QUERY_MAX_LENGTH \
+            or (query.startswith('@') and len(split) == 1):
+        title = f"{emoji.FIRE} Fsıltı Mesajı Yaz"
+        content = ("**Satır içi fısıltı mesajları gönderin**\n\n"
+                   "Kullanım: `@OzelMesajBot [@username] yazı`")
+        description = "Kullanım: @OzelMesajBot [@username] yazı"
+        thumb_url = WHISPER_ICON_URL
+        button = InlineKeyboardButton(
+            "Daha fazla bilgi edin..",
+            url="https://t.me/OzelmesajBot"
+        )
+        switch_pm_text = f"{emoji.INFORMATION} Fısıltıları nasıl göndereceğinizi öğrenin"
+        switch_pm_parameter = "learn"
+    elif not query.startswith('@'):
+        title = f"{emoji.EYE} İlk açana bir kez fısılda"
+        content = (
+            f"{emoji.EYE} Fısıltıyı ilk açan okuyabilir"
+        )
+        description = f"{emoji.SHUSHING_FACE} {query}"
+        button = InlineKeyboardButton(
+            f"{emoji.EYE} Mesajı göster",
+            callback_data="show_whisper"
+        )
+        thumb_url, switch_pm_text, switch_pm_parameter = None, None, None
+    else:
+        u_target = split[0]
+        title = f"{emoji.LOCKED} Bir fısıltı mesajı {u_target}"
+        content = f"{emoji.LOCKED} Bir fısıltı mesajı {u_target}"
+        description = f"{emoji.SHUSHING_FACE} {split[1]}"
+        button = InlineKeyboardButton(
+            f"{emoji.LOCKED_WITH_KEY} Mesajı göster",
+            callback_data="show_whisper"
+        )
+        thumb_url, switch_pm_text, switch_pm_parameter = None, None, None
+    await iq.answer(
+        results=[
+            InlineQueryResultArticle(
+                title=title,
+                input_message_content=InputTextMessageContent(content),
+                description=description,
+                thumb_url=thumb_url,
+                reply_markup=InlineKeyboardMarkup([[button]])
+            )
+        ],
+        switch_pm_text=switch_pm_text,
+        switch_pm_parameter=switch_pm_parameter
+    )
+
+
+@Client.on_chosen_inline_result()
+async def chosen_inline_result(_, cir: ChosenInlineResult):
+    query = cir.query
+    split = query.split(' ', 1)
+    len_split = len(split)
+    if len_split == 0 or len(query) > ANSWER_CALLBACK_QUERY_MAX_LENGTH \
+            or (query.startswith('@') and len(split) == 1):
+        return
+    if len_split == 2 and query.startswith('@'):
+        # Python 3.9+
+        # receiver_uname, text = split[0].removeprefix('@'), split[1]
+        receiver_uname, text = (
+            split[0][1:] if split[0].startswith('@') else split[0], split[1]
+        )
+    else:
+        receiver_uname, text = None, query
+    sender_uid = cir.from_user.id
+    inline_message_id = cir.inline_message_id
+    whispers[inline_message_id] = {
+        'sender_uid': sender_uid,
+        'receiver_uname': receiver_uname,
+        'text': text
+    }
+
+
+@Client.on_callback_query(filters.regex("^show_whisper$"))
+async def answer_cq(_, cq: CallbackQuery):
+    inline_message_id = cq.inline_message_id
+    if not inline_message_id or inline_message_id not in whispers:
+        try:
+            await cq.answer("Fısıltı metnini bulamıyorum", show_alert=True)
+            await cq.edit_message_text(f"{emoji.NO_ENTRY} Geçersiz fısıltı")
+        except (MessageIdInvalid, MessageNotModified):
+            pass
+        return
+    else:
+        whisper = whispers[inline_message_id]
+        sender_uid = whisper['sender_uid']
+        receiver_uname: Optional[str] = whisper['receiver_uname']
+        whisper_text = whisper['text']
+        from_user: User = cq.from_user
+        if receiver_uname and from_user.username \
+                and from_user.username.lower() == receiver_uname.lower():
+            await read_the_whisper(cq)
+            return
+        if from_user.id == sender_uid:
+            await cq.answer(whisper_text, show_alert=True)
+            return
+        if not receiver_uname:
+            await read_the_whisper(cq)
+            return
+        await cq.answer("Bu senin için değil", show_alert=True)
+
+
+async def read_the_whisper(cq: CallbackQuery):
+    inline_message_id = cq.inline_message_id
+    whisper = whispers[inline_message_id]
+    whispers.pop(inline_message_id, None)
+    whisper_text = whisper['text']
+    await cq.answer(whisper_text, show_alert=True)
+    receiver_uname: Optional[str] = whisper['receiver_uname']
+    from_user: User = cq.from_user
+    user_mention = (
+        f"{from_user.first_name} (@{from_user.username})"
+        if from_user.username
+        else from_user.mention
+    )
+    try:
+        t_emoji = emoji.UNLOCKED if receiver_uname else emoji.EYES
+        await cq.edit_message_text(
+            f"{t_emoji} {user_mention} Mesajı Okudu"
+        )
+    except (MessageIdInvalid, MessageNotModified):
+        pass
---
+++ plugins/private.py
@@ -0,0 +1,150 @@
+"""
+ezWhisperBot, Telegram Bot for sending whisper messages
+Copyright (C) 2021  Dash Eclipse
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from datetime import datetime
+
+from pyrogram import Client, filters, emoji
+from pyrogram.types import (Message,
+                            InlineKeyboardMarkup, InlineKeyboardButton,
+                            CallbackQuery)
+
+from data import whispers
+
+LEARN_TEXT = (
+    "u bot yalnızca satır içi modda çalışır, örnek bir kullanım şöyle olur "
+    "- @username için bir fısıltı yazın\n"
+    "`@ezWhisperBot @username burada biraz metin`\n\n"
+    "- İlk açana fısılda (PM'de de kullanılabilir)\n"
+    "`@ezWhisperBot burada biraz metin`"
+)
+LEARN_REPLY_MARKUP = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "Next",
+                callback_data="learn_next"
+            )
+        ]
+    ]
+)
+
+DEFAULT_TEXT = (
+    "Bu bot, fısıltı mesajları göndermenizi sağlar, "
+    "yalnızca satır içi modda çalışır\n\n"
+    "[Kanal](https://t.me/netdbots)"
+    " | [Developer](https://t.me/Azerbesk)"
+)
+DEFAULT_REPLY_MARKUP = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "Denenecek Bir Sohbet Seçin",
+                switch_inline_query=""
+            ),
+            InlineKeyboardButton(
+                "Bu Sohbette Deneyin",
+                switch_inline_query_current_chat=""
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "Fısıltılarım",
+                callback_data="list_whispers"
+            )
+        ]
+    ]
+)
+
+
+@Client.on_message(filters.text
+                   & filters.incoming
+                   & ~filters.edited
+                   & filters.command("start"))
+async def command_start(_, m: Message):
+    if len(m.command) == 2 and m.command[1] == "learn":
+        text_start = LEARN_TEXT
+        reply_markup = LEARN_REPLY_MARKUP
+    else:
+        text_start = DEFAULT_TEXT
+        reply_markup = DEFAULT_REPLY_MARKUP
+    await m.reply_text(
+        text_start,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
+
+
+@Client.on_callback_query(filters.regex("^(learn_next|start)$"))
+async def show_main_page(_, cq: CallbackQuery):
+    await cq.edit_message_text(
+        text=DEFAULT_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=DEFAULT_REPLY_MARKUP
+    )
+    if cq.data == "learn_next":
+        await cq.answer("Artık satır içi moda dönebilirsiniz")
+
+
+@Client.on_callback_query(filters.regex("^list_whispers$"))
+async def list_whispers(_, cq: CallbackQuery):
+    user_id = cq.from_user.id
+    user_whispers = [
+        i for i in whispers.values() if i['sender_uid'] == user_id
+    ]
+    if len(user_whispers) == 0:
+        text = "Hiç fısıltı yok"
+    else:
+        text = f"Fısıltın Var **{len(user_whispers)}**"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    f"{emoji.WASTEBASKET}  Fısıltılarımı Sil",
+                    callback_data="delete_my_whispers"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    f"{emoji.BACK_ARROW}  Ana Sayfaya Dön",
+                    callback_data="start"
+                )
+            ]
+        ]
+    )
+    await cq.edit_message_text(
+        text=text,
+        reply_markup=reply_markup
+    )
+
+
+@Client.on_callback_query(filters.regex("delete_my_whispers"))
+async def delete_my_whispers(_, cq: CallbackQuery):
+    user_id = cq.from_user.id
+    deleted_whispers = [
+        whispers.pop(k)
+        for k, v in list(whispers.items())
+        if v['sender_uid'] == user_id
+    ]
+    if len(deleted_whispers) == 0:
+        await cq.answer("hiç fısıltı yok")
+    else:
+        await cq.answer(f"{len(deleted_whispers)} Fısıltı Kaldırıldı.")
+        utcnow = datetime.utcnow().strftime('%F %T')
+        await cq.edit_message_text(
+            f"Fısıltılarınız kaldırıldı `{utcnow}`",
+            reply_markup=cq.message.reply_markup
+        )
